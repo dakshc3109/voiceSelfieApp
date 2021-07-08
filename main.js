2dc76b08944a6cd1fd87cfd5d9428a54c7a36f0c
dakshc3109 <78908644+dakshc3109@users.noreply.github.com>
2021-07-08 16:52:20 +0530

    -competing project

--- main.js
+++ main.js
@@ -12,4 +12,44 @@ recognition.onresult = function run(event){
     var content = event.results[0][0].transcript;
     console.log(content);
     document.getElementById("textBox").innerHTML = content;
+
+    if(content == "take my selfie"){
+       console.log("taking selfie-- ");
+        speak();
+    }
+}
+
+function speak(){
+   var synth = window.speechSynthesis;
+  // var speech_data = document.getElementById("textBox").value;
+   var speech_data = "Taking your selfie in 5 seconds";
+   var utter_this = new SpeechSynthesisUtterance(speech_data);
+   synth.speak(utter_this);
+   Webcam.attach(camera);
+
+   setTimeout(function(){
+    take_snapshot();
+    save();
+   },5000);
+}
+
+var camera = document.getElementById("camera");
+Webcam.set({
+    width: 350,
+    height: 250,
+    image_format: "jpeg",
+    jpeg_quality: 90
+});
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id='selfie_img' src='"+data_uri+"'>";
+    });  
+};
+
+function save(){
+    var link = document.getElementById("link");
+    var image = document.getElementById("selfie_img").src;
+    link.href = image;
+    link.click();
 }
